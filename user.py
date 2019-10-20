298fd1f8ec907f740d18709ccd413e4a7c530d0a
Abhi Sinha <sinha45@purdue.edu>
2019-10-20 03:52:44 -0400

    this is why compilers are better than interpreters

--- user.py
+++ user.py
@@ -9,26 +9,29 @@ from __future__ import annotations
 
 class User:
     def __init__(self, name, number):
-        self.name = name
-        self.number = number
-        self.partner = None
+        self._name = name
+        self._number = number
+        self._partner = None
+
+    def name(self):
+        return self._name
+
+    def number(self):
+        return self._number
+
+    def partner(self):
+        return self._partner
+
+    def set_partner(self, partner: User):
+        self._partner = partner
 
     def __str__(self):
-        _s = "User(name: " + self.name + ", number: " + self.number
-        if self.partner is not None:
-            _s += ", partner: " + self.partner.__str__()
+        _s = "User(name: " + self._name + ", number: " + self._number
+        if self._partner is not None:
+            _s += ", partner: " + self._partner.__str__()
         _s += ")"
         return _s
 
-    def name(self):
-        return self.name
-
-    def number(self):
-        return self.number
-
-    def partner(self):
-        return self.partner
-
-    def set_partner(self, partner: User):
-        self.partner = partner
-
+if __name__ == "__main__":
+    j = User("Jar8", "+19739780831")
+    print(j.name())
